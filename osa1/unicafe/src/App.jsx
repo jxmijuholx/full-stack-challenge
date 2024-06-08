def84be572625723ec7074039949fb3d64d6c0d5
jxmijuholx <jami.juhola01@gmail.com>
2024-06-08 16:12:29 +0300

    unicafe steps 2, 3, 4

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -15,10 +15,35 @@ const Statistic = ({ text, value }) => (
   </tr>
 )
 
+const Statistics = ({good, neutral, bad}) =>{
+  const all = good + neutral + bad
+  console.log(all)
+  const average = (good - bad) / all
+  console.log(average)
+  const positive = good / all * 100 + " %"
+  console.log(positive)
+
+  if (all === 0) {
+    return <p>Give feedback by pressing the buttons</p>
+  }
+
+  return (
+    <div>
+      <Statistic text="Good" value={good} />
+      <Statistic text="Neutral" value={neutral} />
+      <Statistic text="Bad" value={bad} />
+      <Statistic text="All" value={all} />
+      <Statistic text="Average" value={average} />
+      <Statistic text="Positive" value={positive} />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
   return (
     <div>
@@ -27,10 +52,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <Header text="Statistics" />
-      <Statistic text="Good" value={good} />
-      <Statistic text="Neutral" value={neutral} />
-      <Statistic text="Bad" value={bad} />
-
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
