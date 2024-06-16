384670c200dcae93b49f5f4007136d3fd742ed11
jxmijuholx <jami.juhola01@gmail.com>
2024-06-16 15:08:02 +0300

    axios

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from './components/Search';
 
 const App = () => {
 
   const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([])
+
+  const hook = () => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        try {
+          setCountries(response.data)
+          console.log('Maat haetttu')
+        } catch (error) {
+          console.log('Wallahi veli ei toimi bro',error)
+        }
+      }), useEffect(hook, [])
+  }
 
   return (
     <div>
