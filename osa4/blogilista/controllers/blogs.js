1e234ee33480bbbec56fd9151b8850184db6a506
jxmijuholx <jami.juhola01@gmail.com>
2024-07-25 23:35:03 +0300

    feature: blogs and users are connected in the db

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,12 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
-    })
+})
 
-blogsRouter.get('/:id', async(request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
     const blog = await Blog.findById(request.params.id)
     if (blog) {
         response.json(blog)
@@ -14,63 +15,74 @@ blogsRouter.get('/:id', async(request, response) => {
         response.status(404).end()
     }
 })
-
 blogsRouter.post('/', async (request, response) => {
-    const body = request.body
+    const { title, author, url, likes = 0, user } = request.body;
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    })
+    // Extract userId from the user object
+    if (!user || !user.id) {
+        return response.status(400).json({ error: 'User ID is required' });
+    }
+    const userId = user.id;
 
-    if (!blog.likes) {
-        blog.likes = 0
-      }
-
-    if (!blog.title || !blog.url) {
-        return response.status(400).json({ error: 'title tai url puuttuu weli' })
+    // Check for missing required fields
+    if (!title) {
+        return response.status(400).json({ error: 'Title is required' });
+    }
+    if (!url) {
+        return response.status(400).json({ error: 'URL is required' });
     }
 
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+    // Check if user exists
+    const userRecord = await User.findById(userId);
+    if (!userRecord) {
+        return response.status(400).json({ error: 'Invalid User ID' });
+    }
+
+    // Create and save the new blog
+    const blog = new Blog({
+        title,
+        author,
+        url,
+        likes,
+        user: userRecord._id
+    });
+
+    const savedBlog = await blog.save();
+    userRecord.blogs = userRecord.blogs.concat(savedBlog._id);
+    await userRecord.save();
+
+    response.status(201).json(savedBlog);
 });
+
+
 
 blogsRouter.delete('/:id', async (request, response) => {
     const blog = await Blog.findByIdAndDelete(request.params.id)
     if (blog) {
-      response.status(204).end()
-    } else if( !blog.id) {
-      response.status(400).json({ error: 'invalid id' })
+        response.status(204).end()
+    } else {
+        response.status(400).json({ error: 'Invalid ID' })
     }
-});
+})
 
-blogsRouter.put('/:id', async (request, response)=> {
-    const body = request.body
-
-    const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    }
-
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+blogsRouter.put('/:id', async (request, response) => {
+    const { title, author, url, likes } = request.body
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        { title, author, url, likes },
+        { new: true }
+    )
     response.status(200).json(updatedBlog)
 })
 
 blogsRouter.patch('/:id', async (request, response) => {
-    const body = request.body
-
-    const blog = {
-        likes: body.likes
-    }
-
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const { likes } = request.body
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        { likes },
+        { new: true }
+    )
     response.status(200).json(updatedBlog)
 })
-
-
 
 module.exports = blogsRouter
---
+++ osa4/blogilista/tests/user_api.test.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcrypt')
+const { test, after, beforeEach, describe, toBeDefined, toBeUndefined } = require('node:test');
+const assert = require('node:assert');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const helper = require('./test_helper');
+const User = require('../models/user');
+
+describe('kun on käyttäjä ;)', () => {
+    beforeEach( async() => {
+        await User.deleteMany({})
+        const passwordHash = await bcrypt.hash('salasana', 10)
+        const user = new User({ username: 'root', passwordHash })
+        await user.save()
+    })
+
+    test('post works' , async () => {
+        const ekat = await helper.usersInDb()
+
+        const newUser = {
+            username: 'jami',
+            name: 'Jami Juhola',
+            password: 'salasana'
+        }
+
+        await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+        const tokat = await helper.usersInDb()
+        expect(tokat).toHaveLength(ekat.length + 1)
+
+        const usernames = tokat.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+    
+        const newUser = {
+          username: 'root',
+          name: 'Superuser',
+          password: 'salainen',
+        }
+    
+        const result = await api
+          .post('/api/users')
+          .send(newUser)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+    
+        const usersAtEnd = await helper.usersInDb()
+        assert(result.body.error.includes('expected `username` to be unique'))
+    
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+      })
+})
